df534d9741103f63f6653b6e27c32ae85222da7b
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-05-20 11:46:56 +0900

    コードは書いた、見直しのみ

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,4 +1,9 @@
+struct Material
+{
+    float32_t4 color;
+};
 
+ConstantBuffer<Material> gMaterial : register(b0);
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
@@ -7,7 +12,7 @@ struct PixelShaderOutput
 PixelShaderOutput main()
 {
     PixelShaderOutput output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    output.color = gMaterial.color;
     return output;
 
 }
--- main.cpp
+++ main.cpp
@@ -438,6 +438,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
 	descriptionRootSignature.Flags =
 	D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+			
+	//RootParameter作成。複数設定できるので配列。今回は結果1つだけなので長さ1の配列
+	D3D12_ROOT_PARAMETER rootParameters[1] = {};
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CBVを使う
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//PixelShaderで使う
+	rootParameters[0].Descriptor.ShaderRegister = 0;//レジスタ番号0とバインド
+	descriptionRootSignature.pParameters = rootParameters;//ルートパレメーター配列へのポインタ
+	descriptionRootSignature.NumParameters = _countof(rootParameters);//配列の長さ
+
 	//シリアライズしてバイナリにする
 	ID3DBlob* signatureBlob = nullptr;
 	ID3DBlob* errorBlob = nullptr;
@@ -577,6 +586,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		scissorRect.right = kClientWidth;
 		scissorRect.top = 0;
 		scissorRect.bottom = kClientHeight;
+		
+		ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizeInBytes);
+
+		ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(Vector4) * 3);
+
+		//マテリアル用のリソースを作る。今回はcolor1つ分のサイズを用意する
+		ID3D12Resource* materialResource = CreateBufferResource(device, sizeof(Vector4));
+		//マテリアルにデータを書き込む
+		Vector4* materialData = nullptr;
+		//書き込むためのアドレスを取得
+		materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+		//今回は赤を書き込んでみる
+		*materialData = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
 
 
 	MSG msg{};
@@ -625,6 +647,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			commandList->IASetVertexBuffers(0, 1, &vertexBufferView);
 			//形状を設定。PSOに設定しているものととはまた別。同じものを設定すると考えておけば良い
 			commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+			
+			//マテリアルCBufferの場所を設定
+			commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+
 			//描画(DrawCall/ドローコール)。3頂点で1つのインスタンス。インスタンスについては今後
 			commandList->DrawInstanced(3, 1, 0, 0);
 
@@ -707,6 +733,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
 		debug->Release();
+		materialResource->Release();
 	}
 
 	return 0;
