daafdf808adcbde27d5baf4747f3247f4d2070f7
オノゲンキ <k024G0005@g.neec.ac.jp>
2025-05-21 18:20:47 +0900

    完全完成

--- main.cpp
+++ main.cpp
@@ -871,27 +871,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			IID_PPV_ARGS(&graphicsPipelineState));
 		assert(SUCCEEDED(hr));
 
-		//頂点リソース用のヒープの設定
-		D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-		uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;//UPloadHeapを使う
-		//頂点リソースの設定
-		D3D12_RESOURCE_DESC vertexResourceDesc{};
-		//バッファリソース。テクスチャの場合はまた別の設定をする
-		vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-		vertexResourceDesc.Width = sizeof(Vector4) * 3;//リリースのサイズ。今回はVector4を3頂点分
-		//バッファの場合はこれらは1にする決まり
-		vertexResourceDesc.Height = 1;
-		vertexResourceDesc.DepthOrArraySize = 1;
-		vertexResourceDesc.MipLevels = 1;
-		vertexResourceDesc.SampleDesc.Count = 1;
-		//バッファの場合はこれにする決まり
-		vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-		//実際に頂点リソースを作る
-	/*	ID3D12Resource* vertexResource = nullptr;*/
-		hr = device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE,
-			&vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
-			IID_PPV_ARGS(&vertexResource));
-		assert(SUCCEEDED(hr));
+		
 
 		//頂点バッファビューを作成する
 		D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
@@ -1075,6 +1055,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	vertexShaderBlob->Release();
 
 	wvpResource->Release();
+	materialResource->Release();
 
 
 	//リリースリークチェック
@@ -1085,7 +1066,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
 		debug->Release();
-		materialResource->Release();
 
 	}
 
