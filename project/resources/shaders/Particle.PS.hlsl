2fdf80759447fe4d2dee2b877eb1dac9ef1a8781
オノゲンキ <k024G0005@g.neec.ac.jp>
2026-03-06 11:35:37 +0900

    シェーダー完了

---
+++ project/resources/shaders/Particle.PS.hlsl
@@ -0,0 +1,53 @@
+#include"Particle.hlsli"
+
+struct Material
+{
+    float32_t4 color;
+    int32_t enableLighting;
+    float32_t4x4 uvTransform;
+};
+
+struct DirectionalLight
+{
+    float32_t4 color; //ライトの色
+    float32_t3 direction; //ライトの向き
+    float intensity; //輝度
+};
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    
+    float4 uv = mul(float4(input.texcoord, 0, 1), gMaterial.uvTransform);
+    float4 tex = gTexture.Sample(gSampler, uv.xy);
+
+    if (gMaterial.enableLighting != 0)
+    {
+        float3 normal = normalize(input.normal);
+        float3 lightDir = normalize(-gDirectionalLight.direction);
+        float NdotL = saturate(dot(normal, lightDir));
+        float3 diffuse = gMaterial.color.rgb * tex.rgb * gDirectionalLight.color.rgb * NdotL * gDirectionalLight.intensity;
+        output.color.rgb = diffuse;
+        output.color.a = gMaterial.color.a * tex.a;
+
+    }
+    else
+    {
+        output.color = gMaterial.color * tex;
+    }
+    
+    if (tex.a <= 0.5f)
+        discard;
+    return output;
+}
---
+++ project/resources/shaders/Particle.VS.hlsl
@@ -0,0 +1,30 @@
+#include"Particle.hlsli"
+
+struct TransformationMatrix
+{
+    float32_t4x4 WVP;
+    float32_t4x4 World;
+};
+StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
+
+struct VertexShaderInput
+{
+    float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
+};
+
+
+VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID)
+{
+    VertexShaderOutput output;
+    
+    float4 worldPos = mul(input.position, gTransformationMatrices[instanceId].World);
+    output.worldPosition = worldPos.xyz;
+
+    output.position = mul(worldPos, gTransformationMatrices[instanceId].WVP);
+
+    output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrices[instanceId].World));
+    return output;
+}
